d8e87a3f513b972c248c63225ccd0486d5d0adf1
tungbt <jsmile1994@gmail.com>
2021-11-20 16:33:26 +0700

    update

---
+++ src/com/app_layout/general_header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// header: (Quick Menu - Rostek) - Search Input - (Mobile App - Account)
+
+export const GeneralHeader = ({
+
+}) => {
+    return (
+        <div style={{ backgroundColor: 'red' }}>
+            Header
+        </div>
+    );
+}
--- src/helper/router/private_route.js
+++ src/helper/router/private_route.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 // REDUX
 import { isLoginSelector } from '../../app_state/login';
 import { ROUTES } from '../../config/route';
+import { GeneralHeader } from '../../com/app_layout/general_header';
 
 function PrivateRoute({ children, ...rest }) {
     const isLogin = useSelector(isLoginSelector);
@@ -13,14 +14,18 @@ function PrivateRoute({ children, ...rest }) {
         <Route
             {...rest}
             render={({ location }) =>
-                isLogin ? (children) : (
-                    <Redirect
-                        to={{
-                            pathname: `/${ROUTES.LOGIN}`,
-                            state: { from: location }
-                        }}
-                    />
-                )
+                isLogin ?
+                    <div>
+                        <GeneralHeader />
+                        {children}
+                    </div> : (
+                        <Redirect
+                            to={{
+                                pathname: `/${ROUTES.LOGIN}`,
+                                state: { from: location }
+                            }}
+                        />
+                    )
             }
         />
     );
