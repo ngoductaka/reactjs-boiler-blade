1bd18ff2aea0ae57c83d417a5371d7bde4558a34
ducdn <ducdn@gmail.com>
2021-11-22 18:47:08 +0700

    update code

--- src/config/end_point.js
+++ src/config/end_point.js
@@ -1,4 +1,4 @@
-const host = process.env.REACT_APP_ENV === 'dev' ? '13.229.146.39' : window.location.host.split(":")[0];
+const host = process.env.REACT_APP_ENV === 'dev' ? '_ip' : window.location.host.split(":")[0];
 
 export const ENDPOINT = {
     BASE: `http://${host}:5010`,
